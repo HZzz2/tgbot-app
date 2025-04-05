5d58c4ce3dcdbf6b176ca6c806c852b0d03fe284
HZzz2 <hzzz2yb@gmail.com>
2025-04-05 17:24:23 +0800

    更新action

--- src/cron.rs
+++ src/cron.rs
@@ -7,16 +7,10 @@ use tgbot_app::{util::REQWEST_CLIENT, GLOBAL_CONFIG};
 pub async fn tianqi(bot: Arc<Bot>) {
     let tianqi:Value = REQWEST_CLIENT.get("https://cn.apihz.cn/api/tianqi/tqyb.php?id=88888888&key=88888888&sheng=湖南&place=长沙")
     .send().await.unwrap().json().await.unwrap();
+    // 公共id key容易失败
     if tianqi["place"] == "null" {
         return;
     }
-    //     let str_format = format!(
-    //         "
-    // *☀️ 天气预报 ☀️*
-    // 🏙️ *地区*: {}
-    // 🌡️ *温度*: {}°C",
-    //         tianqi["place"], tianqi["temperature"]
-    //     );
     let str_format = format!(
         "
     *☀️ 天气预报 ☀️*
